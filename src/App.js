8139f559d8ace6914c045e3df65c3efb930944b5
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2024-09-26 00:21:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useMemo, useEffect} from 'react';
 import logo from './logo.svg';
-import './app.scss'
+import './app.scss' 
 import Trivia from './components/Trivia';
 // import { useState, useMemo } from 'react/cjs/react.development';
 import Timer from './components/Timer';
